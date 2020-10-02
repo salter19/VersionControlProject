67a90f38b77a4110c52be2d0c301a6ee93116b2c
Terhi Salonen <salonenterhi19@gmail.com>
2020-10-02 17:38:18 +0300

    Create skeleton main().

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+This is the entrypoint to this project.
+Please, do not fill main() with functions,
+but rather create new class for each feature.
+*/
+
+const main = () => {
+  console.log('Hello world!')
+}
+
+main()
