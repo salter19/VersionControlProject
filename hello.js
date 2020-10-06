89a31dec17f34e6372c1961183d3c68502d79dee
Lauri Paajarvi <lauri.paajarvi@gmail.com>
2020-10-06 11:38:46 +0300

    fix Hello.js typo of missing } character in sayGoodbye() .

--- hello.js
+++ hello.js
@@ -2,6 +2,9 @@ class Hello {
   static sayHello () {
     console.log('Hey mates! Let\'s rock this boat!')
   }
+  static sayGoodbye() {
+    console.log('Goodbye!');
+  }  
 }
 
 module.exports = Hello
