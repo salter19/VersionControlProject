78b426c0aa86bf3e83bdd9c2f8bfbe77c18496dc
Lauri Paajarvi <lauri.paajarvi@tuni.fi>
2020-10-09 13:09:35 +0300

    Modifies index.js: CRUEL WORLD row is split to 2 rows.

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ const main = () => {
   console.log(MATH.hypotenuse(3, 4))
   console.log(MATH.poweroftwo(3))
 
-  console.log(HELLO.yell(HELLO.getGreeting(parseInt(MATH.sum(40, 2))))+'CRUEL WORLD!')
+  console.log(HELLO.yell(HELLO.getGreeting(parseInt
+  (MATH.sum(40, 2))))+'CRUEL WORLD!')
 }
 
 main()
