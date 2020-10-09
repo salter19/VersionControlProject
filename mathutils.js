63377c34704f285ad14f9dc9c47d4a0c080d9ba5
JenninaFarm <56800256+JenninaFarm@users.noreply.github.com>
2020-10-09 12:27:58 +0300

    Add method in mathutils.js called stemOrHeight().

--- mathutils.js
+++ mathutils.js
@@ -36,6 +36,10 @@ class MathUtils {
     return 'The hypotenuse is' + Math.sqrt(Math.pow(stem, 2) + Math.pow(height, 2))
   }
 
+  static stemOrHeight(hypotenuse, side) {
+    return Math.sqrt(Math.pow(hypotenuse, 2) - Math.pow(side, 2))
+  }
+
   static poweroftwo (a) {
     console.log('Number 2 to the power of ' + a)
     return 'Result is ' + Math.pow(2, a)
