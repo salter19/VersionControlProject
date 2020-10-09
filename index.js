40c18e55255ee909a97d2f2a81b28309590690f8
JenninaFarm <56800256+JenninaFarm@users.noreply.github.com>
2020-10-09 12:36:28 +0300

    Call MATH.stemOrHeight() in index.js.

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const main = () => {
 
   console.log(HELLO.yell('this is a custom message.'))
   console.log(HELLO.yell('help i can\'t stop screaming'))
+  console.log(HELLO.yell(HELLO.getGreeting(MATH.sum(40, 2))) + 'CRUEL WORLD!')
 
   console.log(MATH.sum(5, 5))
   console.log(MATH.sumMore(10, 10, 10))
@@ -27,9 +28,8 @@ const main = () => {
   console.log(MATH.abs(3))
   console.log(MATH.abs(-3))
   console.log(MATH.hypotenuse(3, 4))
+  console.log(MATH.stemOrHeight(5, 3))
   console.log(MATH.poweroftwo(3))
-
-  console.log(HELLO.yell(HELLO.getGreeting(MATH.sum(40, 2)))+'CRUEL WORLD!')
 }
 
 main()
