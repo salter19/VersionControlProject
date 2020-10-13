a99a47c21a6283e098bdf9737812db65a9c89c7c
Terhi <twitchy.witchy.blog@gmail.com>
2020-10-13 12:44:16 +0300

    Remove hello.js import from msgHandler.js

--- msgHandler.js
+++ msgHandler.js
@@ -5,6 +5,7 @@ class MsgHandler {
 
   static getGreeting (caller, input) {
     const value = (!isNaN(input) && Number.isInteger(input)) ? input : -1
+    const _this = HELLO
     let foundIt = false
     let result = -1
 
