8b90f892edc55073ea26dff3fe75bb6c8e63e082
Terhi <twitchy.witchy@gmail.com>
2020-10-13 09:02:24 +0300

    Refactor code regarding hello.js. + Begin to move message handling methods into msgHandler.js. + Begin with getGreeting(a), copy method into msgHandler.js.

--- msgHandler.js
+++ msgHandler.js
@@ -2,6 +2,29 @@
 * Use for handling messages.
 */
 class MsgHandler {
+
+  static getGreeting (input) {
+    const value = (!isNaN(input) && Number.isInteger(input)) ? input : -1
+    let foundIt = false
+    let result = -1
+
+    const options = {
+      1: this.sayCheese(),
+      2: this.sayHello(),
+      3: this.sayGoodnightInGreek(),
+      4: this.sayGoodbye(),
+      666: this.upTheIrons(),
+      42: this.sayGoodbye()
+    }
+
+    // if given value is found in options, foundIt is set true.
+    // if foundIt is already true, it can't be set to false again
+    for (let opt in options) {
+      Number(opt) === value ? foundIt = true
+        : foundIt ? foundIt = true : foundIt = false
+    }
+  }
+
 }
 
 module.exports = MsgHandler
