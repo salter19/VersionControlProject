ff23db8d3b93a26bc20b79d210a3d8b0255b65cd
Lauri Paajarvi <lauri.paajarvi@tuni.fi>
2020-10-13 10:57:54 +0300

    Modify hello.js:Remove yell(). Maybe it didn't work before?.

--- hello.js
+++ hello.js
@@ -24,20 +24,6 @@ class Hello {
     return 'Kalinihta!'
   }
 
-  static yell (method) {
-    const str = method
-    /* if the last character is not "." or "!"
-       then ! will be added
-    */
-    let ending = '!'
-    if (str.charAt(str.length - 1) === '!') {
-      ending = ''
-    } else if (str.charAt(str.length - 1) === '.') {
-      ending = ''
-    }
-    return str.toUpperCase() + ending
-  }
-
   static greet (input) {
     return HANDLER.getGreeting(this, input)
   }
