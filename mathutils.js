c6c74fe1757adbc34ccaa4f5930fe86ee465dce4
JenninaFarm <56800256+JenninaFarm@users.noreply.github.com>
2020-10-14 14:01:17 +0300

    Delete printObj in mathutils.js.

--- mathutils.js
+++ mathutils.js
@@ -71,12 +71,6 @@ class MathUtils {
     return Math.PI * Math.pow(radius, 2)
   }
 
-  static printObj (obj) {
-    for (const key in obj) {
-      console.log(`${key} : ${obj[key]}`)
-    }
-  }
-
   static poweroftwo (a) {
     console.log('Number 2 to the power of ' + a)
     return 'Result is ' + Math.pow(2, a)
