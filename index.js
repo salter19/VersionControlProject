1d1fb9870d9dad2f4e626dac1172c5c323d7d2ca
Lauri Paajarvi <lauri.paajarvi@tuni.fi>
2020-10-15 14:16:23 +0300

    Modify index.js:Change sayCheese() to be yelled 6 times.

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const main = () => {
   console.log(HELLO.greet(42))
   console.log(HELLO.greet(0))
   console.log(UTILS.yell(HELLO.greet(666)))
-  console.log((UTILS.yell(HELLO.greet(1)).repeat(3)))
+  console.log((UTILS.yell(HELLO.greet(1)).repeat(6)))
   console.log(HELLO.feelTheGame().repeat(MATH.sum(2, 40)))
   console.log(UTILS.getBottomBreaker())
 
