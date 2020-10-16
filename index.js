710d442a890f342b0955239a19f12408bb630e89
Lauri Paajarvi <lauri.paajarvi@tuni.fi>
2020-10-16 16:37:07 +0300

    Modify index.js:Change CRUEL WORLD to HELLO WORLD.

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const main = () => {
   console.log(UTILS.getBottomBreaker())
 
   console.log(HANDLER.yell(HANDLER.getGreeting(HELLO, parseInt(
-    MATH.sum(40, 2)))) + ' CRUEL WORLD!')
+    MATH.sum(40, 2)))) + ' AND HELLO WORLD!')
   console.log(HANDLER.getGreeting(HELLO, 85))
   console.log(HANDLER.yell(HANDLER.getGreeting(HELLO, 0)))
   console.log(UTILS.getBottomBreaker().repeat(2))
