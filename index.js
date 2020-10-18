e091713e4eb46587285ecb3d924d3da1a073ef4a
Lauri Paajarvi <lauri.paajarvi@tuni.fi>
2020-10-18 12:58:58 +0300

    Modify index.js:Change 'and hello world' to lowercase letters.

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const main = () => {
   console.log(UTILS.getBottomBreaker())
 
   console.log(HANDLER.yell(HANDLER.getGreeting(HELLO, parseInt(
-    MATH.sum(40, 2)))) + ' AND HELLO WORLD!')
+    MATH.sum(40, 2)))) + ' and hello world.')
   console.log(HANDLER.getGreeting(HELLO, 1985))
   console.log(HANDLER.getGreeting(HELLO, 0))
   console.log(UTILS.getBottomBreaker().repeat(1))
