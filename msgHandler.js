c4e5b339bae81e8425ab7fee53a24519560eb919
Terhi <twitchy.witchy.blog@gmail.com>
2020-10-21 11:58:58 +0300

    Fix eslint errors in msgHandler.js.

--- msgHandler.js
+++ msgHandler.js
@@ -1,14 +1,14 @@
 /**
  * Use for handling messages.
  */
-const UTILS = require("./utils.js");
-const HELLO = require("./hello.js");
+const UTILS = require('./utils.js')
+const HELLO = require('./hello.js')
 
 class MsgHandler {
-  static getGreeting(input) {
-    const value = !isNaN(input) && Number.isInteger(input) ? input : -1;
-    let foundIt = false;
-    let result = -1;
+  static getGreeting (input) {
+    const value = !isNaN(input) && Number.isInteger(input) ? input : -1
+    let foundIt = false
+    let result = -1
 
     const options = {
       0: HELLO.getSayNothing(),
@@ -19,9 +19,9 @@ class MsgHandler {
       5: HELLO.getGoToSleep(),
       666: HELLO.getUpTheIrons(),
       42: HELLO.get42(),
-      1985: this.scream("Long Beach"),
-      2020: this.feelTheGame(),
-    };
+      1985: this.scream('Long Beach'),
+      2020: this.feelTheGame()
+    }
 
     // if given value is found in options, foundIt is set true.
     // if foundIt is already true, it can't be set to false again
@@ -29,40 +29,40 @@ class MsgHandler {
       Number(opt) === value
         ? (foundIt = true)
         : foundIt
-        ? (foundIt = true)
-        : (foundIt = false);
+          ? (foundIt = true)
+          : (foundIt = false)
     }
 
     foundIt
       ? (result = options[value])
-      : (result = "Could not compute given value.");
+      : (result = 'Could not compute given value.')
 
-    return result;
+    return result
   }
 
-  static yell(input) {
-    const str = input;
+  static yell (input) {
+    const str = input
     /* if the last character is not "." or "!"
        then ! will be added
     */
-    let ending = "!";
-    if (str.charAt(str.length - 1) === "!") {
-      ending = "";
-    } else if (str.charAt(str.length - 1) === ".") {
-      ending = "";
+    let ending = '!'
+    if (str.charAt(str.length - 1) === '!') {
+      ending = ''
+    } else if (str.charAt(str.length - 1) === '.') {
+      ending = ''
     }
-    return str.toUpperCase() + ending;
+    return str.toUpperCase() + ending
   }
 
-  static feelTheGame() {
+  static feelTheGame () {
     const result =
-      UTILS.getTopBreaker() + " " + this.scream("Tampere") + "\n - tappara";
-    return this.yell(result);
+      UTILS.getTopBreaker() + ' ' + this.scream('Tampere') + '\n - tappara'
+    return this.yell(result)
   }
 
-  static scream(city) {
-    return this.yell("Scream for me " + city);
+  static scream (city) {
+    return this.yell('Scream for me ' + city)
   }
 }
 
-module.exports = MsgHandler;
+module.exports = MsgHandler
