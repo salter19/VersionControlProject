54597508dbaf301202b46cce37e95d0c7c2a7fe1
Terhi <twitchy.witchy.blog@gmail.com>
2020-10-21 10:56:28 +0300

    Remove getGoodnight() from msgHandler.js.

--- msgHandler.js
+++ msgHandler.js
@@ -1,13 +1,13 @@
 /**
-* Use for handling messages.
-*/
-const UTILS = require('./utils.js')
+ * Use for handling messages.
+ */
+const UTILS = require("./utils.js");
 
 class MsgHandler {
-  static getGreeting (caller, input) {
-    const value = (!isNaN(input) && Number.isInteger(input)) ? input : -1
-    let foundIt = false
-    let result = -1
+  static getGreeting(caller, input) {
+    const value = !isNaN(input) && Number.isInteger(input) ? input : -1;
+    let foundIt = false;
+    let result = -1;
 
     const options = {
       0: caller.getSayNothing(),
@@ -18,50 +18,50 @@ class MsgHandler {
       5: caller.getGoToSleep(),
       666: caller.getUpTheIrons(),
       42: caller.get42(),
-      1985: this.scream('Long Beach'),
-      2020: this.feelTheGame()
-    }
+      1985: this.scream("Long Beach"),
+      2020: this.feelTheGame(),
+    };
 
     // if given value is found in options, foundIt is set true.
     // if foundIt is already true, it can't be set to false again
     for (const opt in options) {
-      Number(opt) === value ? foundIt = true
-        : foundIt ? foundIt = true : foundIt = false
+      Number(opt) === value
+        ? (foundIt = true)
+        : foundIt
+        ? (foundIt = true)
+        : (foundIt = false);
     }
 
-    foundIt ? result = options[value] : result = 'Could not compute given value.'
+    foundIt
+      ? (result = options[value])
+      : (result = "Could not compute given value.");
 
-    return result
+    return result;
   }
 
-  static yell (input) {
-    const str = input
+  static yell(input) {
+    const str = input;
     /* if the last character is not "." or "!"
        then ! will be added
     */
-    let ending = '!'
-    if (str.charAt(str.length - 1) === '!') {
-      ending = ''
-    } else if (str.charAt(str.length - 1) === '.') {
-      ending = ''
+    let ending = "!";
+    if (str.charAt(str.length - 1) === "!") {
+      ending = "";
+    } else if (str.charAt(str.length - 1) === ".") {
+      ending = "";
     }
-    return str.toUpperCase() + ending
+    return str.toUpperCase() + ending;
   }
 
-  static feelTheGame () {
-    const result = UTILS.getTopBreaker() + ' ' + this.scream('Tampere') + '\n - tappara'
-    return this.yell(result)
+  static feelTheGame() {
+    const result =
+      UTILS.getTopBreaker() + " " + this.scream("Tampere") + "\n - tappara";
+    return this.yell(result);
   }
 
-  static scream (city) {
-    return this.yell('Scream for me ' + city)
+  static scream(city) {
+    return this.yell("Scream for me " + city);
   }
-
-/*
-  static goToSleep () {
-    return 'Well, nighty night then!'
-  }
-*/
 }
 
-module.exports = MsgHandler
+module.exports = MsgHandler;
