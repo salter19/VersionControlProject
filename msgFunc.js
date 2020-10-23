9ace2e192e6f144e6f3232f5375141f24d95809f
Terhi <twitchy.witchy.blog@gmail.com>
2020-10-23 10:25:29 +0300

    Fix MsgFunc object's syntax. + Remove failing lines following func1... + Create key func1 in correct place.

--- msgFunc.js
+++ msgFunc.js
@@ -2,13 +2,12 @@ const MsgFunc = function (city) {
   if (this instanceof MsgFunc) {
     this.city = city;
     console.log("how do yo do");
+    this.func1 = function () {
+      return "do you do, the things that you do";
+    };
   } else {
     throw new TypeError("Please use keyword new.");
   }
-
-  func1: () => {
-    return "Here is the MsgFunc!";
-  };
 };
 
 module.exports = MsgFunc;
