30e93a99dccbed0a4a50ce0a4dce9dc72d337391
JenninaFarm <56800256+JenninaFarm@users.noreply.github.com>
2020-10-23 11:40:30 +0300

    Call in idex.js: MATH.innerCircleRadius().

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ const main = () => {
   )
   console.log(MATH.triangleAngle(40, 30))
   console.log(MATH.eclipseArea(3, 5))
+  console.log(MATH.innerCircleRadius(3, 4, 5, MATH.rightTriagleArea(3, 4)))
 }
 
 main()
