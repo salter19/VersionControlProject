34a5492c9a9559ada4d6e8cde9354f7d49df4224
Terhi <twitchy.witchy.blog@gmail.com>
2020-10-23 10:30:42 +0300

    Create key func2 into MsgFunc object in msgFunc.js.

--- msgFunc.js
+++ msgFunc.js
@@ -5,6 +5,9 @@ const MsgFunc = function (city) {
     this.func1 = function () {
       return "do you do, the things that you do";
     };
+    this.func2 = function () {
+      return "Finally I got the freaking syntax right!";
+    };
   } else {
     throw new TypeError("Please use keyword new.");
   }
