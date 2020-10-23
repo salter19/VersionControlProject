499233d7cdc4312e7434776b2104744079c1f992
Terhi <twitchy.witchy.blog@gmail.com>
2020-10-23 10:33:56 +0300

    Fix eslint errors in msgFunc.js, msgHandler.js and index.js.

--- msgFunc.js
+++ msgFunc.js
@@ -1,16 +1,16 @@
 const MsgFunc = function (city) {
   if (this instanceof MsgFunc) {
-    this.city = city;
-    console.log("how do yo do");
+    this.city = city
+    console.log('how do yo do')
     this.func1 = function () {
-      return "do you do, the things that you do";
-    };
+      return 'do you do, the things that you do'
+    }
     this.func2 = function () {
-      return "Finally I got the freaking syntax right!";
-    };
+      return 'Finally I got the freaking syntax right!'
+    }
   } else {
-    throw new TypeError("Please use keyword new.");
+    throw new TypeError('Please use keyword new.')
   }
-};
+}
 
-module.exports = MsgFunc;
+module.exports = MsgFunc
