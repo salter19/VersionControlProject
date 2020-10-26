55cdfab2d8512ba598e1db58d9fc536eaa6c0154
Terhi <twitchy.witchy.blog@gmail.com>
2020-10-26 18:38:53 +0200

    Fix eslint errors in msgFunc.js.

--- msgFunc.js
+++ msgFunc.js
@@ -1,16 +1,16 @@
 class MsgFunc {
-  constructor(city) {
-    this.city = city;
-    this.func1 = this.func1.bind(this);
+  constructor (city) {
+    this.city = city
+    this.func1 = this.func1.bind(this)
   }
 
-  func1() {
-    return `Hello ${this.city}`;
+  func1 () {
+    return `Hello ${this.city}`
   }
 
-  func2() {
-    return "Finally I got the freaking syntax right!";
+  func2 () {
+    return 'Finally I got the freaking syntax right!'
   }
 }
 
-module.exports = MsgFunc;
+module.exports = MsgFunc
