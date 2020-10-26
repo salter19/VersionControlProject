b1b08d670fccf532a76c965781717a96f3e1511a
Terhi <twitchy.witchy.blog@gmail.com>
2020-10-26 18:35:39 +0200

    Remove delayedFunc1 from msgFunc.js

--- msgFunc.js
+++ msgFunc.js
@@ -1,23 +1,16 @@
 class MsgFunc {
-  constructor (city) {
-    this.city = city
-    this.func1 = this.func1.bind(this)
+  constructor(city) {
+    this.city = city;
+    this.func1 = this.func1.bind(this);
   }
 
-  func1 () {
-    return `Hello ${this.city}`
+  func1() {
+    return `Hello ${this.city}`;
   }
 
-  func2 () {
-    return 'Finally I got the freaking syntax right!'
-  }
-
-  delayedFunc1 () {
-    setTimeout(this.func1, 2000)
-  }
-
-  func3 () {
-    return this.func1
+  func2() {
+    return "Finally I got the freaking syntax right!";
   }
 }
-module.exports = MsgFunc
+
+module.exports = MsgFunc;
