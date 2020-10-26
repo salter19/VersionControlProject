19e86599c116aed616fd54e2bed9b458744942e6
Terhi <twitchy.witchy.blog@gmail.com>
2020-10-26 16:42:11 +0200

    Battle and fail with asynchronic stuff in msgFunc.js.

--- msgFunc.js
+++ msgFunc.js
@@ -1,19 +1,23 @@
 class MsgFunc {
   constructor(city) {
     this.city = city;
-    console.log("how do yo do");
+    this.func1 = this.func1.bind(this);
   }
 
-  static func1() {
-    return "do you do, the things that you do";
+  func1() {
+    return `Hello ${this.city}`;
   }
 
-  static func2() {
+  func2() {
     return "Finally I got the freaking syntax right!";
   }
 
-  static delayedFunc1() {
-    setTimeout(() => this.func1, 2000);
+  delayedFunc1() {
+    setTimeout(this.func1, 2000);
+  }
+
+  func3() {
+    return this.func1;
   }
 }
 module.exports = MsgFunc;
